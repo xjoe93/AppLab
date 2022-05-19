a234959eedc155d5eca82100fca889e4fc995f8f
unknown <xjoestar93@gmail.com>
2022-05-19 09:25:53 +0200

    Hello App

---
+++ script.js
@@ -0,0 +1,61 @@
+let navBtn = document.getElementById("list");
+
+function show() {
+    if (navBtn.style.display === "block") {
+        navBtn.style.display = "none";
+    } else {
+        navBtn.style.display = "block";
+    }
+};
+
+
+
+// Questions Section
+
+let questionBtn = document.getElementById("question-btn");
+
+let answer = document.getElementById("answer");
+let answer2 = document.getElementById("answer2");
+let answer3 = document.getElementById("answer3");
+let answer4 = document.getElementById("answer4");
+let answer5 = document.getElementById("answer5");
+
+function showQuestion() {
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+    } else {
+        answer.style.display = "block";
+    }
+};
+
+function showQuestion2() {
+    if (answer2.style.display === "block") {
+        answer2.style.display = "none";
+    } else {
+        answer2.style.display = "block";
+    }
+};
+
+function showQuestion3() {
+    if (answer3.style.display === "block") {
+        answer3.style.display = "none";
+    } else {
+        answer3.style.display = "block";
+    }
+};
+
+function showQuestion4() {
+    if (answer4.style.display === "block") {
+        answer4.style.display = "none";
+    } else {
+        answer4.style.display = "block";
+    }
+};
+
+function showQuestion5() {
+    if (answer5.style.display === "block") {
+        answer5.style.display = "none";
+    } else {
+        answer5.style.display = "block";
+    }
+};
